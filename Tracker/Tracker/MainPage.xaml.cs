aa8f8c9ffdae4c9992ad6568954ed567795426db
yanak <yana.kapeluhova@gmail.com>
2021-11-19 11:27:57 -0500

    Fixed the conversion to JArray instead of JObject plus a little correction in assigning the value for id. Now everything should work properly.

--- Tracker/Tracker/MainPage.xaml.cs
+++ Tracker/Tracker/MainPage.xaml.cs
@@ -21,23 +21,26 @@ namespace Tracker
 
         private async void changeToMostRecentRecord()
         {
-            try
-            {
+            
                 string url = "https://louenes099.pythonanywhere.com/pi/api/get/last";
                 var handler = new HttpClientHandler();
                 HttpClient client = new HttpClient(handler);
                 string result = await client.GetStringAsync(url);
-                var resultsObject = JObject.Parse(result);
+
+                Console.WriteLine(result);
+           // result = result.TrimStart(new char[] { '[' }).TrimEnd(new char[] { ']' });
+            var resultsObject = JArray.Parse(result);
 
                 Console.WriteLine(resultsObject);
 
-                int id = resultsObject[0][1].ToObject<int>();
+                int id = resultsObject[0][0].ToObject<int>();
                 string time = resultsObject[0][1].ToString();
                 double temp = resultsObject[0][2].ToObject<double>();
                 double hum = resultsObject[0][3].ToObject<double>();
 
                 r = new Record(id, time, temp, hum);
-
+            try
+            {
                 xtime.Text = r.dateTime;
                 xtemp.Text = r.Temp.ToString() + "°C";
                 xhum.Text = r.Hum.ToString() + "%";
