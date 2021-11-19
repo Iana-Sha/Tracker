fc7dd6419acbeebe079f73a54535c0c953745b1f
yanak <yana.kapeluhova@gmail.com>
2021-11-18 20:15:02 -0500

    Added try and catch for when json data couldn't be retrieved

--- Tracker/Tracker/MainPage.xaml.cs
+++ Tracker/Tracker/MainPage.xaml.cs
@@ -21,29 +21,32 @@ namespace Tracker
 
         private async void changeToMostRecentRecord()
         {
-            string url = "file:///C:/test/data.json";
-            //string url = "https://louenes099.pythonanywhere.com/pi/api/get/last";
-            var handler = new HttpClientHandler();
-            HttpClient client = new HttpClient(handler);
-            string result = await client.GetStringAsync(url);
-            var resultsObject = JObject.Parse(result);
+            try
+            {
+                string url = "https://louenes099.pythonanywhere.com/pi/api/get/last";
+                var handler = new HttpClientHandler();
+                HttpClient client = new HttpClient(handler);
+                string result = await client.GetStringAsync(url);
+                var resultsObject = JObject.Parse(result);
 
-            Console.WriteLine(resultsObject);
+                Console.WriteLine(resultsObject);
 
-            int id = resultsObject[0][1].ToObject<int>();
-            string time = resultsObject[0][1].ToString();
-            double temp = resultsObject[0][2].ToObject<double>();
-            double hum = resultsObject[0][3].ToObject<double>();
+                int id = resultsObject[0][1].ToObject<int>();
+                string time = resultsObject[0][1].ToString();
+                double temp = resultsObject[0][2].ToObject<double>();
+                double hum = resultsObject[0][3].ToObject<double>();
 
+                r = new Record(id, time, temp, hum);
 
-            
-            r = new Record(id, time, temp, hum);
-
-            xtime.Text = r.dateTime;
-            xtemp.Text = r.Temp.ToString() + "°C";
-            xhum.Text = r.Hum.ToString() + "%";
-            xfanStatus.Text = r.FanOn == true ? "On" : "Off";
-            
+                xtime.Text = r.dateTime;
+                xtemp.Text = r.Temp.ToString() + "°C";
+                xhum.Text = r.Hum.ToString() + "%";
+                xfanStatus.Text = r.FanOn == true ? "On" : "Off";
+            }
+            catch(Exception e)
+            {
+                warning.Text = "Error occured while getting data: " + e;
+            }
         }
     }
 }
